d7a802a061a2811881b3452e1e32787cf5ebb9ab
cristofflanders <cristofflanders@gmail.com>
2013-01-16 08:50:37 +0000

    Partial Javadoc info added.

--- src/tonegod/gui/controls/buttons/CheckBox.java
+++ src/tonegod/gui/controls/buttons/CheckBox.java
@@ -22,6 +22,13 @@ public class CheckBox extends Button {
 	
 	Label label;
 	
+	/**
+	 * Creates a new instance of the CheckBox control
+	 * 
+	 * @param screen The screen control the Element is to be added to
+	 * @param UID A unique String identifier for the Element
+	 * @param position A Vector2f containing the x/y position of the Element
+	 */
 	public CheckBox(Screen screen, String UID, Vector2f position) {
 		this(screen, UID, position,
 			screen.getStyle("CheckBox").getVector2f("defaultSize"),
@@ -30,6 +37,14 @@ public class CheckBox extends Button {
 		);
 	}
 	
+	/**
+	 * Creates a new instance of the CheckBox control
+	 * 
+	 * @param screen The screen control the Element is to be added to
+	 * @param UID A unique String identifier for the Element
+	 * @param position A Vector2f containing the x/y position of the Element
+	 * @param dimensions A Vector2f containing the width/height dimensions of the Element
+	 */
 	public CheckBox(Screen screen, String UID, Vector2f position, Vector2f dimensions) {
 		this(screen, UID, position, dimensions,
 			screen.getStyle("CheckBox").getVector4f("resizeBorders"),
@@ -37,6 +52,16 @@ public class CheckBox extends Button {
 		);
 	}
 	
+	/**
+	 * Creates a new instance of the CheckBox control
+	 * 
+	 * @param screen The screen control the Element is to be added to
+	 * @param UID A unique String identifier for the Element
+	 * @param position A Vector2f containing the x/y position of the Element
+	 * @param dimensions A Vector2f containing the width/height dimensions of the Element
+	 * @param resizeBorders A Vector4f containg the border information used when resizing the default image (x = N, y = W, z = E, w = S)
+	 * @param defaultImg The default image to use for the Slider's track
+	 */
 	public CheckBox(Screen screen, String UID, Vector2f position, Vector2f dimensions, Vector4f resizeBorders, String defaultImg) {
 		super(screen, UID, position, dimensions, resizeBorders, defaultImg);
 		
