997725052c5c0cd7eb676afa115c7105455a95ff
cristofflanders <cristofflanders@gmail.com>
2013-01-16 08:17:23 +0000

    Javadoc info added.

--- src/tonegod/gui/controls/text/Label.java
+++ src/tonegod/gui/controls/text/Label.java
@@ -17,6 +17,14 @@ import tonegod.gui.core.Screen;
  */
 public class Label extends Element {
 	
+	/**
+	 * Creates a new instance of the Label control
+	 * 
+	 * @param screen The screen control the Element is to be added to
+	 * @param UID A unique String identifier for the Element
+	 * @param position A Vector2f containing the x/y position of the Element
+	 * @param dimensions A Vector2f containing the width/height dimensions of the Element
+	 */
 	public Label(Screen screen, String UID, Vector2f position, Vector2f dimensions) {
 		this(screen, UID, position, dimensions,
 			screen.getStyle("Label").getVector4f("resizeBorders"),
@@ -24,6 +32,16 @@ public class Label extends Element {
 		);
 	}
 	
+	/**
+	 * Creates a new instance of the Label control
+	 * 
+	 * @param screen The screen control the Element is to be added to
+	 * @param UID A unique String identifier for the Element
+	 * @param position A Vector2f containing the x/y position of the Element
+	 * @param dimensions A Vector2f containing the width/height dimensions of the Element
+	 * @param resizeBorders A Vector4f containg the border information used when resizing the default image (x = N, y = W, z = E, w = S)
+	 * @param defaultImg The default image to use for the Slider's track
+	 */
 	public Label(Screen screen, String UID, Vector2f position, Vector2f dimensions, Vector4f resizeBorders, String defaultImg) {
 		super(screen, UID, position, dimensions, resizeBorders, defaultImg);
 		
