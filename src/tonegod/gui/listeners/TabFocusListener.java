36b7aa288c79bd65c8d2b8d0504441211b0cb7b8
cristofflanders <cristofflanders@gmail.com>
2013-01-23 01:56:29 +0000

    Updates all relating to forms and tab focus / default keyboard interaction for controls.  More to come.

---
+++ src/tonegod/gui/listeners/TabFocusListener.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tonegod.gui.listeners;
+
+/**
+ *
+ * @author t0neg0d
+ */
+public interface TabFocusListener {
+	void setTabFocus();
+	void resetTabFocus();
+}
