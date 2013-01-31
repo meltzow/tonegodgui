22983506d907dcf0bcee9b7d44f5cd8ad5d73495
cristofflanders <cristofflanders@gmail.com>
2013-01-31 22:46:16 +0000

    Cleanup

--- src/tonegod/gui/core/Screen.java
+++ src/tonegod/gui/core/Screen.java
@@ -20,6 +20,7 @@ import com.jme3.math.Ray;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.math.Vector4f;
+import com.jme3.renderer.Camera;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Node;
