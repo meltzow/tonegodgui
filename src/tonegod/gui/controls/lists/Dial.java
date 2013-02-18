26be17c1ec6818ef617a37a275a79b13f14b7b53
cristofflanders <cristofflanders@gmail.com>
2013-02-18 18:25:24 +0000

    Dial control has been overhauled

--- src/tonegod/gui/controls/lists/Dial.java
+++ src/tonegod/gui/controls/lists/Dial.java
@@ -8,6 +8,8 @@ import com.jme3.math.FastMath;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.math.Vector4f;
+import com.jme3.scene.Geometry;
+import com.jme3.texture.Texture;
 import java.util.ArrayList;
 import java.util.List;
 import tonegod.gui.controls.buttons.ButtonAdapter;
@@ -21,14 +23,18 @@ import tonegod.gui.effects.Effect;
  */
 public abstract class Dial extends ButtonAdapter {
 	Element elCenter, elPosition;
-	protected List<String> stepValues = new ArrayList();
+	protected List<Object> stepValues = new ArrayList();
 	
 	int selectedIndex = 0;
 	boolean isStepped = false;
 	float stepSize = 1;
 	float currentAngle = 0;
-	float gap = 30;
+	float startGap, endGap, totalGap, totalSize;
 	
+	float minDegrees = 0;
+	float maxDegrees = 359;
+	
+	Texture indicatorTex;
 	/**
 	 * Creates a new instance of the Dial control
 	 * 
@@ -38,9 +44,9 @@ public abstract class Dial extends ButtonAdapter {
 	 */
 	public Dial(Screen screen, String UID, Vector2f position) {
 		this(screen, UID, position,
-			screen.getStyle("Button").getVector2f("defaultSize"),
-			screen.getStyle("Button").getVector4f("resizeBorders"),
-			screen.getStyle("Button").getString("defaultImg")
+			screen.getStyle("Dial").getVector2f("defaultSize"),
+			screen.getStyle("Dial").getVector4f("resizeBorders"),
+			screen.getStyle("Dial").getString("defaultImg")
 		);
 	}
 	
@@ -54,8 +60,8 @@ public abstract class Dial extends ButtonAdapter {
 	 */
 	public Dial(Screen screen, String UID, Vector2f position, Vector2f dimensions) {
 		this(screen, UID, position, dimensions,
-			screen.getStyle("Button").getVector4f("resizeBorders"),
-			screen.getStyle("Button").getString("defaultImg")
+			screen.getStyle("Dial").getVector4f("resizeBorders"),
+			screen.getStyle("Dial").getString("defaultImg")
 		);
 	}
 	
@@ -67,7 +73,7 @@ public abstract class Dial extends ButtonAdapter {
 	 * @param position A Vector2f containing the x/y position of the Element
 	 * @param dimensions A Vector2f containing the width/height dimensions of the Element
 	 * @param resizeBorders A Vector4f containg the border information used when resizing the default image (x = N, y = W, z = E, w = S)
-	 * @param defaultImg The default image to use for the Slider's track
+	 * @param defaultImg The default image to use for the Dial's background image
 	 */
 	public Dial(Screen screen, String UID, Vector2f position, Vector2f dimensions, Vector4f resizeBorders, String defaultImg) {
 		super(screen, UID, position, dimensions, resizeBorders, defaultImg);
@@ -76,32 +82,57 @@ public abstract class Dial extends ButtonAdapter {
 		removeEffect(Effect.EffectEvent.Press);
 		removeEffect(Effect.EffectEvent.LoseFocus);
 		
+		startGap = minDegrees;
+		endGap = 359-maxDegrees;
+		totalGap = startGap + endGap;
+		totalSize = 359-totalGap;
+		
 		elCenter = new Element(
 			screen,
 			UID + ":Center",
-			new Vector2f(getWidth()/2, 0),
-			new Vector2f(1, getHeight()/2),
+			new Vector2f((getWidth()/2), -(getHeight()/2)),
+			new Vector2f(dimensions.x, dimensions.y),
 			new Vector4f(0,0,0,0),
-			null
+			screen.getStyle("Dial").getString("radialImg")
 		);
+		((Geometry)elCenter.getChild(0)).center();
 		elCenter.setScaleNS(false);
 		elCenter.setScaleEW(false);
 		elCenter.setDockS(true);
 		elCenter.setDockW(true);
+		elCenter.setIgnoreMouse(true);
 		addChild(elCenter);
 		
-		elPosition = new Element(
-			screen,
-			UID + ":Position",
-			new Vector2f(-6, 0),
-			new Vector2f(12, 12),
-			new Vector4f(0,0,0,0),
-			screen.getStyle("Common").getString("arrowUp")
-		);
-		elCenter.addChild(elPosition);
-		elPosition.setIgnoreMouse(true);
-		
+		setStepSize();
 		setInterval(100);
+	}
+	
+	/**
+	 * Sets the angle at which the minimum rotation of the dial will start (0 being the bottom)
+	 * @param angle float
+	 */
+	public void setGapStartAngle(int angle) {
+		if (angle < 0) angle = 0;
+		minDegrees = angle;
+		startGap = minDegrees;
+		endGap = 359-maxDegrees;
+		totalGap = startGap + endGap;
+		totalSize = 359-totalGap;
+		setStepSize();
+	}
+	
+	/**
+	 * Sets the angle at which the maximum rotation of the dial will stop (359 being the bottom)
+	 * @param angle float
+	 */
+	public void setGapEndAngle(int angle) {
+		if (angle > 359) angle = 359;
+		maxDegrees = angle;
+		startGap = minDegrees;
+		endGap = 359-maxDegrees;
+		totalGap = startGap + endGap;
+		totalSize = 359-totalGap;
+		setStepSize();
 	}
 
 	/**
@@ -112,12 +143,11 @@ public abstract class Dial extends ButtonAdapter {
 	 * 
 	 * @param value The string value to add for the next step.
 	 */
-	public void addStepValue(String value) {
+	public void addStepValue(Object value) {
 		stepValues.add(value);
 		if (stepValues.size() >= 2) {
 			isStepped = true;
 			setStepSize();
-		//	this.setInterval(1);
 		}
 	}
 	
@@ -126,31 +156,104 @@ public abstract class Dial extends ButtonAdapter {
 	 * 
 	 * @param value The string value of the step to be removed.
 	 */
-	public void removeStepValue(String value) {
+	public void removeStepValue(Object value) {
 		stepValues.remove(value);
 		if (stepValues.size() < 2) {
 			isStepped = false;
 			setStepSize();
-		//	this.setInterval(100);
 		}
+	}
+	
+	/**
+	 * Sets the Dial's rotating indicator image
+	 * @param imgPath String Path to image
+	 */
+	public void setDialImageIndicator(String imgPath) {
+		indicatorTex = screen.getApplication().getAssetManager().loadTexture(imgPath);
+		indicatorTex.setMinFilter(Texture.MinFilter.BilinearNoMipMaps);
+		indicatorTex.setMagFilter(Texture.MagFilter.Bilinear);
+		indicatorTex.setWrap(Texture.WrapMode.Repeat);
+		
+		elCenter.getElementMaterial().setTexture("ColorMap", indicatorTex);
+	}
+	
+	/**
+	 * Sets the Dial's background image
+	 * @param imgPath String Path to image
+	 */
+	public void setDialImageBackground(String imgPath) {
+		Texture tex = screen.getApplication().getAssetManager().loadTexture(imgPath);
+		tex.setMinFilter(Texture.MinFilter.BilinearNoMipMaps);
+		tex.setMagFilter(Texture.MagFilter.Bilinear);
+		tex.setWrap(Texture.WrapMode.Repeat);
+		
+		getElementMaterial().setTexture("ColorMap", tex);
 	}
 	
 	private void setStepSize() {
-		stepSize = (360-(gap*2))/(stepValues.size()-1);
-	//	System.out.println(stepSize);
+		if (stepValues.size() >= 2)
+			stepSize = totalSize/(stepValues.size()-1);
+		else
+			stepSize = totalSize/100;
 	}
 	
 	private float getStepAngle(float angle) {
-		angle += 180;
+		angle += 180-startGap;
 		
-		int nIndex = (int)Math.floor(angle/stepSize);
-		
-		if (nIndex >= 0 && nIndex < stepValues.size() && nIndex != this.selectedIndex) {
-		//	System.out.println(nIndex);
-			setSelectedIndex(nIndex);
+		int nIndex;
+		if (stepValues.size() >= 2) {
+			nIndex = (int)Math.round(angle/stepSize);
+			if (nIndex >= 0 && nIndex < stepValues.size() && nIndex != this.selectedIndex)
+				setInternalIndex(nIndex);
+		} else {
+			nIndex = (int)Math.round(angle/stepSize);
+			int finIndex = (int)Math.round(nIndex);
+			if (finIndex != this.selectedIndex)
+				setInternalIndex(finIndex);
 		}
-		
-		return gap+(nIndex*stepSize)-180;
+
+		return (nIndex*stepSize)-180+startGap;
+	}
+	
+	/**
+	 * Sets the selected index for both free-floating and stepped Dials
+	 * @param index float
+	 */
+	public void setSelectedIndex(int index) {
+		float angle;
+		if (isStepped) {
+			if (index < 0) index = 0;
+			else if (index > stepValues.size()-1) index = stepValues.size()-1;
+			setInternalIndex(index);
+			angle = index*stepSize-180+startGap;
+		} else {
+			if (index < 0) index = 0;
+			else if (index > 100) index = 100;
+			setInternalIndex(index);
+			angle = index*stepSize-180+startGap;
+		}
+		elCenter.setLocalRotation(elCenter.getLocalRotation().fromAngleAxis(-(angle*FastMath.DEG_TO_RAD), Vector3f.UNIT_Z));
+	}
+	
+	/**
+	 * For use with free-floating Dials - Sets the selected index of the Dial
+	 * @param index float A range from 0.0 to 100.0 for a more accurate representation of the angle desired
+	 */
+	public void setSelectedIndex(float index) {
+		float angle;
+		int index1 = (int)Math.round(index);
+		if (isStepped) {
+			if (index1 < 0) index1 = 0;
+			else if (index1 > stepValues.size()-1) index1 = stepValues.size()-1;
+			setInternalIndex(index1);
+			angle = index1*stepSize-180+startGap;
+		} else {
+			if (index1 < 0) index1 = 0;
+			else if (index1 > 100) index1 = 100;
+			setInternalIndex(index1);
+			angle = index*stepSize-180+startGap;
+		}
+		elCenter.setLocalRotation(elCenter.getLocalRotation().fromAngleAxis(-(angle*FastMath.DEG_TO_RAD), Vector3f.UNIT_Z));
 	}
 	
 	/**
@@ -168,10 +271,10 @@ public abstract class Dial extends ButtonAdapter {
 	 * 
 	 * @param selectedIndex The index to set the Dial's selectedIndex to.
 	 */
-	private void setSelectedIndex(int selectedIndex) {
+	private void setInternalIndex(int selectedIndex) {
 		this.selectedIndex = selectedIndex;
-		if( isStepped)	onChange(selectedIndex, stepValues.get(selectedIndex));
-		else			onChange(selectedIndex, String.valueOf(selectedIndex));
+		if(isStepped)	onChange(selectedIndex, stepValues.get(selectedIndex));
+		else			onChange(selectedIndex, selectedIndex);
 	}
 	
 	/**
@@ -180,21 +283,20 @@ public abstract class Dial extends ButtonAdapter {
 	 * @param selectedIndex The Slider's current selectedIndex
 	 * @param value The string value associated with this index
 	 */
-	public abstract void onChange(int selectedIndex, String value);
+	public abstract void onChange(int selectedIndex, Object value);
 	
 	@Override
 	public void onButtonStillPressedInterval() {
-		Vector2f pos = new Vector2f(elCenter.getAbsoluteX(), elCenter.getAbsoluteY());
 		currentAngle = (float)Math.atan2(screen.getMouseXY().x-elCenter.getAbsoluteX(), screen.getMouseXY().y-elCenter.getAbsoluteY())*FastMath.RAD_TO_DEG;
-		if (currentAngle < -180+gap)
-			currentAngle = -180+gap;
-		else if (currentAngle > 180-gap)
-			currentAngle = 180-gap;
+		
+		if (currentAngle < -(180-startGap))
+			currentAngle = -(180-startGap);
+		else if (currentAngle > (180-endGap))
+			currentAngle = (180-endGap);
+		
 		float angle = Float.valueOf(currentAngle);
 		
-		if (isStepped) {
-			angle = getStepAngle(angle);
-		}
+		angle = getStepAngle(angle);
 		
 		elCenter.setLocalRotation(elCenter.getLocalRotation().fromAngleAxis(-(angle*FastMath.DEG_TO_RAD), Vector3f.UNIT_Z));
 	}
