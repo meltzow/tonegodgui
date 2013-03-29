185bb3df566ab41bc498442e70b1cdfa4e97b92e
cristofflanders <cristofflanders@gmail.com>
2013-03-29 17:40:38 +0000

    Util class for generating random UIDs

---
+++ src/tonegod/gui/core/utils/UIDUtil.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tonegod.gui.core.utils;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author t0neg0d
+ */
+public class UIDUtil {
+	public static String getUID() {
+		UUID uuid = UUID.randomUUID();
+		return uuid.toString();
+	}
+}
