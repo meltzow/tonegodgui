997050e5c79e331f0ed6ee8eecfc3fda77810d1e
cristofflanders <cristofflanders@gmail.com>
2013-11-05 18:07:45 +0000

    Updates from putting the emitter through it's paces.  Lots of groovy stuff.

--- src/tonegod/gui/controls/extras/emitter/DestinationInfluencer.java
+++ src/tonegod/gui/controls/extras/emitter/DestinationInfluencer.java
@@ -11,31 +11,32 @@ import tonegod.gui.controls.extras.emitter.ElementEmitter.ElementParticle;
  *
  * @author t0neg0d
  */
-public class DirectionInfluencer extends InfluencerBase {
+public class DestinationInfluencer extends InfluencerBase {
 	
 	private boolean isEnabled = true;
-	private Vector2f direction = Vector2f.ZERO.clone();
+	private Vector2f destination = Vector2f.ZERO.clone();
 	private Vector2f temp = new Vector2f();
 	private float strength = 1;
 	
-	public DirectionInfluencer(ElementEmitter emitter) {
+	public DestinationInfluencer(ElementEmitter emitter) {
 		super(emitter);
 	}
 	
 	@Override
 	public void update(ElementParticle p, float tpf) {
-		
-	}
-
-	@Override
-	public void initialize(ElementParticle particle) {
 		if (isEnabled) {
-			if (direction != Vector2f.ZERO) {
-				temp.set(direction.normalize()).multLocal(particle.randforce*strength);
-				particle.velocity.interpolate(particle.velocity, temp, 0.5f);
+			if (destination != Vector2f.ZERO) {
+				temp.set(destination.mult(tpf));
+				temp.mult(strength);
+				p.velocity.subtractLocal(temp);
+				temp.set(p.velocity).multLocal(tpf);
+				p.position.addLocal(temp);
 			}
 		}
 	}
+
+	@Override
+	public void initialize(ElementParticle particle) {  }
 
 	@Override
 	public void setIsEnabled(boolean isEnabled) {
@@ -47,12 +48,12 @@ public class DirectionInfluencer extends InfluencerBase {
 		return this.isEnabled;
 	}
 	
-	public void setDirection(Vector2f direction) {
-		this.direction.set(direction);
+	public void setDestination(Vector2f direction) {
+		this.destination.set(direction);
 	}
 	
-	public Vector2f getDirection() {
-		return this.direction;
+	public Vector2f getDestination() {
+		return this.destination;
 	}
 	
 	public void setStrength(float strength) {
@@ -64,9 +65,9 @@ public class DirectionInfluencer extends InfluencerBase {
 	}
 	
 	@Override
-	public DirectionInfluencer clone() {
-		DirectionInfluencer clone = new DirectionInfluencer(emitter);
-		clone.setDirection(direction);
+	public DestinationInfluencer clone() {
+		DestinationInfluencer clone = new DestinationInfluencer(emitter);
+		clone.setDestination(destination);
 		clone.setStrength(strength);
 		return clone;
 	}
--- src/tonegod/gui/controls/extras/emitter/RotationInfluencer.java
+++ src/tonegod/gui/controls/extras/emitter/RotationInfluencer.java
@@ -5,6 +5,7 @@
 package tonegod.gui.controls.extras.emitter;
 
 import com.jme3.math.FastMath;
+import com.jme3.math.Vector2f;
 import tonegod.gui.controls.extras.emitter.ElementEmitter.ElementParticle;
 
 /**
@@ -16,6 +17,10 @@ public class RotationInfluencer extends InfluencerBase {
 	private float maxRotationSpeed;
 	private boolean useRandomStartAngle = true;
 	private boolean useFixedRotationSpeed = false;
+	private boolean rotateFromEmitterPosition = false;
+	private boolean rotateToVelocity = false;
+	private Vector2f tempV2a = new Vector2f();
+	private Vector2f tempV2b = new Vector2f();
 	
 	public RotationInfluencer(ElementEmitter emitter) {
 		super(emitter);
@@ -24,13 +29,26 @@ public class RotationInfluencer extends InfluencerBase {
 	@Override
 	public void update(ElementParticle particle, float tpf) {
 		if (isEnabled) {
-			if (particle.rotateDir)
-				particle.angle += particle.rotateSpeed * tpf * FastMath.RAD_TO_DEG;
-			else
-				particle.angle -= particle.rotateSpeed * tpf * FastMath.RAD_TO_DEG;
+			if (rotateFromEmitterPosition) {
+				tempV2a.set(
+					emitter.getPositionX(),
+					emitter.getPositionY()
+				);
+				tempV2b.set(particle.position);
+				particle.angle = getRotationBetween(
+					tempV2a, tempV2b
+				)+90;
+			} else if (rotateToVelocity) {
+				particle.angle = getRotationFromVelocity(particle.velocity);
+			} else {
+				if (particle.rotateDir)
+					particle.angle += particle.rotateSpeed * tpf * FastMath.RAD_TO_DEG;
+				else
+					particle.angle -= particle.rotateSpeed * tpf * FastMath.RAD_TO_DEG;
+			}
 		}
 	}
-
+	
 	@Override
 	public void initialize(ElementParticle particle) {
 		if (useRandomStartAngle)
@@ -53,11 +71,21 @@ public class RotationInfluencer extends InfluencerBase {
 		return this.isEnabled;
 	}
 	
+	public void setRotateFromEmitterPosition(boolean rotateFromEmitterPosition) {
+		this.rotateFromEmitterPosition = rotateFromEmitterPosition;
+		if (this.rotateToVelocity) this.rotateToVelocity = false;
+	}
+	
+	public void setRotateToVelocity(boolean rotateToVelocity) {
+		this.rotateToVelocity = rotateToVelocity;
+		if (this.rotateFromEmitterPosition) this.rotateFromEmitterPosition = false;
+	}
+	
 	public void setUseFixedRotationSpeed(boolean useFixedRotationSpeed) {
 		this.useFixedRotationSpeed = useFixedRotationSpeed;
 	}
 	
-	public boolean getUseFixedRotationSpped() {
+	public boolean getUseFixedRotationSpeed() {
 		return this.useFixedRotationSpeed;
 	}
 	
@@ -72,6 +100,21 @@ public class RotationInfluencer extends InfluencerBase {
 	}
 	
 	public boolean getUseRandomStartAngle() { return useRandomStartAngle; }
+	
+	private float getRotationBetween(Vector2f v1, Vector2f v2) {
+		float deltaY = v2.y - v1.y;
+		float deltaX = v2.x - v1.x;
+		
+		return FastMath.atan2(deltaY,deltaX) * FastMath.RAD_TO_DEG;
+	}
+	
+	private float getRotationFromVelocity(Vector2f velocity) {
+		tempV2a.set(velocity).normalizeLocal();
+		float angle = FastMath.atan2(tempV2a.y, tempV2a.x)*FastMath.RAD_TO_DEG;
+		angle += 90;
+		return angle;
+		
+	}
 	
 	@Override
 	public RotationInfluencer clone() {
