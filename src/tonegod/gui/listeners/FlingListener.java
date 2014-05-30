cf1b0ab7448fdfd446f2914ebf52c2030533a114
cristofflanders <cristofflanders@gmail.com>
2014-05-30 03:49:51 +0000

    Android fling event listener

---
+++ src/tonegod/gui/listeners/FlingListener.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tonegod.gui.listeners;
+
+import com.jme3.input.event.TouchEvent;
+
+/**
+ *
+ * @author t0neg0d
+ */
+public interface FlingListener {
+	void onFling(TouchEvent evt);
+}
